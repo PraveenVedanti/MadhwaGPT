1d8bc08b8d74c0475cd1a4a6c9e808a03bf239ec
Praveen Vedanti <52764246+PraveenVedanti@users.noreply.github.com>
2026-01-17 22:05:37 -0800

    Initial Commit

---
+++ MadhwaGPT/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MadhwaGPT
+//
+//  Created by Praveen Kumar Vedanti on 1/17/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
